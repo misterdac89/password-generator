9d9c5b9c9fbe049e2ba0254fe275d05bb1e57125
Louis Daccarett <lsdaccarett89@icloud.com>
2023-11-10 07:38:11 -0500

    Password-generator app files
    A web app that is capable of generating passwords.

---
+++ main.js
@@ -0,0 +1,25 @@
+import { characters } from "./data.js";
+
+const generatorBtn = document.getElementById("generator-btn");
+const passwordOne = document.getElementById("passwordOne");
+const passwordTwo = document.getElementById("passwordTwo");
+
+const passwordLength = 15;
+
+function getRandomCharacter() {
+  const randomChar = Math.floor(Math.random() * characters.length);
+  return characters[randomChar];
+}
+
+function getRandomPassword() {
+  let randomPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += getRandomCharacter();
+  }
+  return randomPassword;
+}
+
+generatorBtn.addEventListener("click", function () {
+  passwordOne.textContent = getRandomPassword();
+  passwordTwo.textContent = getRandomPassword();
+});
